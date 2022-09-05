f8a2705e3fc000d7b94cb2e14341dfa8baa5fc2c
Mario Riascos <mriascos1201@gmail.com>
2022-09-05 16:05:49 -0500

    Conversor 1.1

--- conversor.py
+++ conversor.py
@@ -2,6 +2,7 @@ pesos = input("¿Cuántos pesos colombianos tienes?: ")
 pesos = float(pesos) #Transformamos el contenido a decimal 
 valor_dolar = 4466
 dolares = pesos/valor_dolar
+dolares = round(dolares,2)
 dolares = str(dolares)
 
-print("Tienes $" + dolares + "dólares")
+print("Tienes $" + dolares + " dólares")

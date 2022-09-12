fecc4c7fc178fea4f5ba44083210ece9ef7e09c1
Mario Riascos <mriascos1201@gmail.com>
2022-09-12 14:39:23 -0500

    Recorriendo un String con For

---
+++ recorrer.py
@@ -0,0 +1,11 @@
+def run():
+    # name = input('type your name: ')
+    # for letter in name:
+    #     print(letter)
+    prhase = input('type a prhase: ')
+
+    for character in prhase:
+        print(character.upper())       
+
+if __name__ == "__main__":
+    run()
